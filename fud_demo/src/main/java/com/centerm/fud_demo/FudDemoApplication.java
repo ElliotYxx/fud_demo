326d54e97bffc20561402c93cb72267113e0ff8b
ouyangyi1998 <1347401764@qq.com>
2020-01-16 14:54:14 +0800

    test

---
+++ fud_demo/src/main/java/com/centerm/fud_demo/FudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.centerm.fud_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FudDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FudDemoApplication.class, args);
+    }
+
+}
---
+++ fud_demo/src/test/java/com/centerm/fud_demo/FudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centerm.fud_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FudDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
