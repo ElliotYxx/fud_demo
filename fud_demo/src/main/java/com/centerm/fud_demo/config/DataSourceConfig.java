acbbd3d0806d914f2e00bb327e336f6136f9480d
ouyangyi1998 <1347401764@qq.com>
2020-01-17 16:45:21 +0800

    druid数据库连接池更新

--- fud_demo/src/main/java/com/centerm/fud_demo/config/DataSourceConfig.java
+++ fud_demo/src/main/java/com/centerm/fud_demo/config/DataSourceConfig.java
@@ -1,7 +1,133 @@
 package com.centerm.fud_demo.config;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import com.github.pagehelper.PageInterceptor;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
+@MapperScan(basePackages =DataSourceConfig.PACKAGE,sqlSessionFactoryRef = "sqlSessionFactory")
 public class DataSourceConfig {
+
+    public static final String PACKAGE="com.centerm.fud_demo.dao";
+    public static final String MAPPER_LOCATION="classpath:mapper/*.xml";
+
+    @Value("${master.datasource.url}")
+    private String url;
+
+    @Value("${master.datasource.username}")
+    private String username;
+
+    @Value("${master.datasource.password}")
+    private String password;
+
+    @Value("${master.datasource.driverClassName}")
+    private String driverClassName;
+
+
+    @Value("${spring.datasource.initialSize}")
+    private int initialSize;
+
+    @Value("${spring.datasource.minIdle}")
+    private int minIdle;
+
+    @Value("${spring.datasource.maxActive}")
+    private int maxActive;
+
+    @Value("${spring.datasource.maxWait}")
+    private int maxWait;
+
+    @Value("${spring.datasource.timeBetweenEvictionRunsMillis}")
+    private int timeBetweenEvictionRunsMillis;
+
+    @Value("${spring.datasource.minEvictableIdleTimeMillis}")
+    private int minEvictableIdleTimeMillis;
+
+    @Value("${spring.datasource.validationQuery}")
+    private String validationQuery;
+
+    @Value("${spring.datasource.testWhileIdle}")
+    private boolean testWhileIdle;
+
+    @Value("${spring.datasource.testOnBorrow}")
+    private boolean testOnBorrow;
+
+    @Value("${spring.datasource.testOnReturn}")
+    private boolean testOnReturn;
+
+    @Value("${spring.datasource.poolPreparedStatements}")
+    private boolean poolPreparedStatements;
+
+    @Value("${spring.datasource.maxPoolPreparedStatementPerConnectionSize}")
+    private int maxPoolPreparedStatementPerConnectionSize;
+
+    @Value("${spring.datasource.filters}")
+    private String filters;
+
+    @Value("{spring.datasource.connectionProperties}")
+    private String connectionProperties;
+
+    @Bean(name = "dataSource")
+    public DataSource dataSource()
+    {
+        DruidDataSource dataSource=new DruidDataSource();
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setDriverClassName(driverClassName);
+
+        //具体配置
+        dataSource.setInitialSize(initialSize);
+        dataSource.setMinIdle(minIdle);
+        dataSource.setMaxActive(maxActive);
+        dataSource.setMaxWait(maxWait);
+        dataSource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+        dataSource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+        dataSource.setValidationQuery(validationQuery);
+        dataSource.setTestWhileIdle(testWhileIdle);
+        dataSource.setTestOnBorrow(testOnBorrow);
+        dataSource.setTestOnReturn(testOnReturn);
+        dataSource.setPoolPreparedStatements(poolPreparedStatements);
+        dataSource.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPreparedStatementPerConnectionSize);
+        try {
+            dataSource.setFilters(filters);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        dataSource.setConnectionProperties(connectionProperties);
+        return dataSource;
+    }
+    @Bean
+    public DataSourceTransactionManager dataSourceTransactionManager()
+    {
+        return new DataSourceTransactionManager(dataSource());
+    }
+    @Bean(name = "sqlSessionFactory")
+    public SqlSessionFactory sqlSessionFactory(@Qualifier("dataSource")DataSource dataSource) throws Exception
+    {
+        final SqlSessionFactoryBean sessionFactoryBean=new SqlSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource);
+        sessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(DataSourceConfig.MAPPER_LOCATION));
+        Interceptor interceptor=new PageInterceptor();
+        Properties properties=new Properties();
+        properties.setProperty("helperDialect","mysql");
+        properties.setProperty("offsetAsPageNum","true");
+        properties.setProperty("rowBoundWithCount","true");
+        properties.setProperty("reasonable","true");
+        interceptor.setProperties(properties);
+        sessionFactoryBean.setPlugins(new Interceptor[]{interceptor});
+        return sessionFactoryBean.getObject();
+    }
+
 }
